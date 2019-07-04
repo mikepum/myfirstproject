694e0f329e297d591355dfd3cc1a3caae26b480d
codenation <codenation@codenations-MacBook-Pro.local>
2019-07-04 15:13:44 +0100

    new js

---
+++ numberofhis.js
@@ -0,0 +1,19 @@
+let string = "jrfndklhgfndjkjlkgperfijfhidknsadcvjhiiohjfkledsopiuhgtyujwsdxcvhgfdjhiopiwquhiejkdsoiufghedjwsh";
+
+console.log("First hi - " + string.indexOf("hi"));
+console.log("last hi - " + string.lastIndexOf("hi"));
+
+nonefound = false;
+let lastIndex = 0;
+let found = 0;
+
+while(!nonefound) {
+    if (string.indexOf("hi", lastIndex) != -1) {
+        console.log(string.indexOf("hi", lastIndex));
+        lastIndex = string.indexOf("hi", lastIndex) + 1;
+        found++;      
+    } else {
+        nonefound = true;
+    }
+}
+console.log(found);
